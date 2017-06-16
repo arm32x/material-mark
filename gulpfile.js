c4037695a26bac5e94802a43b6ea1847ab69aefc
ARM32 <arm32.x@gmail.com>
2017-06-16 12:23:51 -0400

    Add SASS compilation to Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require("gulp");
 const srcmaps = require("gulp-sourcemaps");
 const babel = require("gulp-babel");
 const concat = require("gulp-concat");
+const sass = require("gulp-sass");
 
 gulp.task("default", () => {
     // Compile JS files
@@ -13,6 +14,15 @@ gulp.task("default", () => {
         .pipe(concat("main.js"))
         .pipe(srcmaps.write("."))
         .pipe(gulp.dest("js"));
+    
+    // Compile SCSS files
+    gulp.src("./*.scss", { base: "./scss" })
+        .pipe(srcmaps.init())
+        .pipe(sass({
+            outputStyle: "compressed"
+        }).on("error", sass.logError))
+        .pipe(srcmaps.write("."))
+        .pipe(gulp.dest("../css"));
     
     // Copy everything to /var/www/html
     gulp.src("**/*", { base: "." })
