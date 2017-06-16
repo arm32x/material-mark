e6c50261cddc30de48ff77e7a5684cae78a6c582
ARM32 <arm32.x@gmail.com>
2017-06-16 10:30:36 -0400

    Clean up the main JS file

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
+// Initialize the ace editor
 var editor = ace.edit("editor");
+
+// Set the theme and language mode for the editor
 editor.setTheme("ace/theme/tomorrow_night");
 editor.getSession().setMode("ace/mode/markdown");
+
+// Set scrolling margins for the editor
 editor.renderer.setScrollMargin(56, 0);
 editor.setOption("scrollPastEnd", 1);
-var timeout;
+
+// Load stored content into the editor
 if (window.location.hash == "" || window.location.hash == "#") {
     editor.getSession().setValue(LZString.decompressFromUTF16(window.localStorage.getItem("data")));
 } else {
     editor.getSession().setValue(LZString.decompressFromBase64(window.location.hash.substr(1)));
 }
+
+// Parse the value that was just loaded
+var value = editor.getSession().getValue();
+document.getElementById("output").innerHTML = marked(value, {
+    gfm: true,
+    tables: false,
+    breaks: true,
+    smartypants: true
+});
+
+// Enable live preview and autosave
+var timeout;
 editor.getSession().on("change", function() {
     window.clearTimeout(timeout);
+    
+    // Get and parse the value from the editor
     var value = editor.getSession().getValue();
     document.getElementById("output").innerHTML = marked(value, {
         gfm: true,
@@ -18,6 +38,8 @@ editor.getSession().on("change", function() {
         breaks: true,
         smartypants: true
     });
+    
+    // Save the editor value into the window hash and localStorage
     window.location.hash = LZString.compressToBase64(value);
     timeout = window.setTimeout(function() {
         window.localStorage.setItem("data", LZString.compressToUTF16(value));
